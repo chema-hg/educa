97bc15674481c5edbe01f86bf1523d06dd6ffa41
chema-hg <honrajm@yahoo.es>
2024-03-31 22:40:58 +0200

    31 construyendo una API django_rest_framework

---
+++ api_examples/enroll_all.py
@@ -0,0 +1,22 @@
+import requests
+
+# Poner el nombre y contraseña de un usuario registrado.
+username = ''
+password = ''
+base_url = 'http://127.0.0.1:8000/api/'
+
+# devuelve todos los cursos disponibles en la base de datos
+r = requests.get(f'{base_url}courses/')
+# trasforma el formato json en un diccionario python    
+courses = r.json()
+
+available_courses = ', '.join([course['title'] for course in courses])
+print(f'Available courses: {available_courses}')
+
+for course in courses:
+    course_id = course['id']
+    course_title = course['title']
+    r = requests.post(f'{base_url}courses/{course_id}/enroll/', auth=(username, password))
+    if r.status_code == 200:
+        # successful request
+        print(f'Successfully enrolled in {course_title}')
--- educa/settings.py
+++ educa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'embed_video',
     'debug_toolbar',
     'redisboard',
+    'rest_framework',
     # aplicaciones por defecto
     'django.contrib.admin',
     'django.contrib.auth',
@@ -160,3 +161,9 @@ INTERNAL_IPS = ['127.0.0.1', ]
 CACHE_MIDDLEWARE_ALIAS = 'default'
 CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutos
 CACHE_MIDDLEWARE_KEY_PREFIX = 'educa'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
